549cb428adcba8f9c477daeea2fe66bf3aff908d
phentality <pheonix09@yandex.ru>
2023-09-22 22:09:32 +0500

    1

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -9,18 +9,17 @@ const cors = (req, res, next) => {
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credential', true);
-    const { method } = req;
-
-    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-    if (method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      // eslint-disable-next-line no-undef
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-      return res.end();
-    }
   }
+  const { method } = req;
 
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // eslint-disable-next-line no-undef
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
   next();
 };
 
