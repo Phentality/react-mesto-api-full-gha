330b2639ad59dd8a5bdbbcd063903e5733f29b45
phentality <pheonix09@yandex.ru>
2023-09-23 14:01:23 +0500

    2

--- backend/app.js
+++ backend/app.js
@@ -17,10 +17,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 const { PORT = 3000 } = process.env;
 
 const app = express();
-const corsOptions = {
-  origin: 'http://localhost:3000',
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(helmet());
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
