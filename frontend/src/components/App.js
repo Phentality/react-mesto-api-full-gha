94d4d3fbcd1f2147e7b0b0311f7f604b34a97549
phentality <pheonix09@yandex.ru>
2023-09-23 12:09:21 +0500

    1

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -37,7 +37,7 @@ function App() {
       .catch((err) => {
         console.log(err);
       })
-  }, []);
+  }, [loggedIn]);
 
   React.useEffect(() => {
     api.getUserInfo()
@@ -47,7 +47,7 @@ function App() {
       .catch((err) => {
         console.log(err);
       })
-  }, []);
+  }, [loggedIn]);
 
   function handleEditProfileClick() {
     setIsEditProfilePopupOpen(!isEditProfilePopupOpen);
