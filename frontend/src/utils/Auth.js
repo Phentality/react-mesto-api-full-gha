bdedc661e6fbe9fe48544dd3c46090fb23f8a6b2
phentality <pheonix09@yandex.ru>
2023-09-24 19:18:07 +0500

    1итерация

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -10,7 +10,6 @@ const checkRes = (res) => {
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
-    credentials: 'include',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
