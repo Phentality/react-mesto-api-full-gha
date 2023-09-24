109da9e1b77251663f6e8093c9b12981bea2c312
phentality <pheonix09@yandex.ru>
2023-09-24 18:49:34 +0500

    1Iter

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-// eslint-disable-next-line no-unused-expressions
-process.env.NODE_ENV !== 'production';
 const { PORT = 3000 } = process.env;
 
 const app = express();
